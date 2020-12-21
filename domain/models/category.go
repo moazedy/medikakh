39ae034a6406a530de62619e1f76801524b17c97
moazedy <moazedy@gitlab.lafarrerr.ir>
2020-12-21 13:36:52 -0500

    adding new model (category) to models

--- domain/models/category.go
+++ domain/models/category.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/google/uuid"
+
 type Category struct {
+
 	Id            uuid.UUID `json:"id"`
 	Name          string    `json:"name"`
 	SubCategories []string  `json:"sub_categories"`
