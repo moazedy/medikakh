ee60ca9c082c4f88a4659dc74fcedebcca105ea8
moazedy <moazedy@gitlab.lafarrerr.ir>
2020-12-26 15:33:23 -0500

    changing articles structure and making them more modular

--- domain/models/article.go
+++ domain/models/article.go
@@ -1,12 +1,19 @@
 package models
 
+import "github.com/google/uuid"
+
 type Article struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Status      string `json:"status"`
-	Summery     string `json:"summery"`
-	Content     string `json:"content"`
-	Result      string `json:"result"`
-	Category    string `json:"category"`
-	SubCategory string `json:"sub_category"`
+	Id               uuid.UUID `json:"id"`
+	Title            string    `json:"title"`
+	Status           string    `json:"status"`
+	Summary          string    `json:"summary"`
+	Etiology         string    `json:"etiology"`
+	ClinicalFeatures string    `json:"clinical_features"`
+	Diagnostics      string    `json:"diagnostics"`
+	Treatment        string    `json:"treatment"`
+	Complications    string    `json:"complications"`
+	Prevention       string    `json:"prevention"`
+	References       string    `json:"references"`
+	Category         string    `json:"category"`
+	SubCategory      string    `json:"sub_category"`
 }
