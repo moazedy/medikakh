61cc2ec164718f3c0688c72b07b085b0a5adbbc0
moazedy <moazedy@gitlab.lafarrerr.ir>
2020-12-26 15:30:13 -0500

    adding constants package to project

---
+++ domain/constants/constants.go
@@ -0,0 +1,32 @@
+package constants
+
+// general constants for casbin
+const (
+	SaveAction   = "save"
+	ReadAction   = "read"
+	DeleteAction = "delete"
+	UpdateAction = "update"
+)
+
+// consts related to articles
+const (
+	ArticleObject = "article"
+)
+
+// related to video
+const (
+	VideoObject = "video"
+)
+
+// related to user
+const (
+	UserObject = "user"
+)
+
+// related to role
+const (
+	BronzeUserObject    = "bronze"
+	SilverUserObject    = "silver"
+	GoldUserObject      = "gold"
+	AnonymousUserObject = "anonymous"
+)
