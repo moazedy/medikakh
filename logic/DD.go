089196a7487fadaf0fcfbd0f8edf573b878a9d01
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-01-10 16:38:39 -0500

    creating dd logic and adding insert, read and read by pattern functions

---
+++ logic/DD.go
@@ -0,0 +1,79 @@
+package logic
+
+import (
+	"errors"
+	"medikakh/domain/constants"
+	"medikakh/domain/models"
+	"medikakh/repository"
+	"medikakh/service/authorization"
+
+	"github.com/google/uuid"
+)
+
+type DDLogic interface {
+	InsertData(userRole string, dd models.DDmodel) error
+	ReadDD(title string) (*models.DDmodel, error)
+	ReadDDbyPattern(pattern string) ([]string, error)
+}
+
+type dd struct {
+	repo repository.DDrepo
+}
+
+func NewDDLogic(repo repository.DDrepo) DDLogic {
+	d := new(dd)
+	d.repo = repo
+
+	return d
+}
+
+func (d *dd) InsertData(userRole string, dd models.DDmodel) error {
+	roleCorrectness := checkForRoleStatmentCorrectness(userRole)
+	if !roleCorrectness {
+		return errors.New("role statment is invalid")
+	}
+
+	// checking for user premissins on saving articles
+	ok := authorization.IsPermissioned(userRole, constants.DDobject, constants.SaveAction)
+	if !ok {
+		return errors.New("premission denied")
+	}
+
+	if dd.Title == "" || dd.Content == nil {
+		return errors.New("title or content, can not be empty")
+	}
+
+	dd.Id = uuid.New()
+	err := d.repo.InsertData(dd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *dd) ReadDD(title string) (*models.DDmodel, error) {
+	if title == "" {
+		return nil, errors.New("title can not be empty")
+	}
+
+	return d.repo.ReadDataByTitle(title)
+}
+
+func (d *dd) ReadDDbyPattern(pattern string) ([]string, error) {
+	if pattern == "" {
+		return nil, errors.New("title can not be empty")
+	}
+
+	dds, err := d.repo.ReadDataUsingPattern(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	titles := make([]string, len(dds.Titles))
+	for i, v := range dds.Titles {
+		titles[i] = v.Title
+	}
+
+	return titles, nil
+}
