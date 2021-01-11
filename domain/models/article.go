00af5e975a982e2ed994bd1f58a830a98d455c4d
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-01-11 14:06:34 -0500

    adding articleUpdate model to article models

--- domain/models/article.go
+++ domain/models/article.go
@@ -17,3 +17,23 @@ type Article struct {
 	Category         string    `json:"category"`
 	SubCategory      string    `json:"sub_category"`
 }
+
+type ArticleUpdate struct {
+	Id               uuid.UUID `json:"id"`
+	Title            *string   `json:"title,omitempty"`
+	Status           *string   `json:"status,omitempty"`
+	Summary          *string   `json:"summary,omitempty"`
+	Etiology         *string   `json:"etiology,omitempty"`
+	ClinicalFeatures *string   `json:"clinical_features,omitempty"`
+	Diagnostics      *string   `json:"diagnostics,omitempty"`
+	Treatment        *string   `json:"treatment,omitempty"`
+	Complications    *string   `json:"complications,omitempty"`
+	Prevention       *string   `json:"prevention,omitempty"`
+	References       *string   `json:"references,omitempty"`
+	Category         *string   `json:"category,omitempty"`
+	SubCategory      *string   `json:"sub_category,omitempty"`
+}
+
+type ArticleTitle struct {
+	Title string `json:"title"`
+}
