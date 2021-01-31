77a9670e98a18aee005ae90b4acb5cd79ffa459b
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-01-31 17:34:21 -0500

    error handling

--- service/authorization/authorization.go
+++ service/authorization/authorization.go
@@ -2,12 +2,29 @@ package authorization
 
 import (
 	"log"
+	"path/filepath"
+	"runtime"
 
 	"github.com/casbin/casbin"
 )
 
 func IsPermissioned(role, object, action string) bool {
-	enforcer := casbin.NewEnforcer("auth_model.conf", "policy.csv")
+	baseFileName := "basic_model.conf"
+	policyFileName := "basic_policy.csv"
+	_, b, _, _ := runtime.Caller(0)
+	basePath := filepath.Dir(b)
+	policyPath := basePath + "/" + baseFileName
+	modelPath := basePath + "/" + policyFileName
+
+	enforcer, err := casbin.NewEnforcerSafe(modelPath, policyPath)
+	if err != nil {
+		log.Println(err.Error())
+		return false
+	}
+	if enforcer == nil {
+		log.Println("failed to open casbin files")
+		return false
+	}
 	res := enforcer.Enforce(role, object, action)
 
 	if !res {
