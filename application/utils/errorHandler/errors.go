c61e81e9f28abf5c605e9c90faf1d28adfdf4e68
moazedy <moazedy@gitlab.lafarrerr.ir>
2021-02-16 16:34:41 -0500

    adding some codes to error handlers

--- application/utils/errorHandler/errors.go
+++ application/utils/errorHandler/errors.go
@@ -1,5 +1,45 @@
 package errorHandler
 
-var ErrorTexts map[string]string
+import "net/http"
 
-type ErrorCodes map[string]int
+type ErrorText map[string]string
+
+type ErrorCode map[string]int
+
+type Errors struct {
+	FA ErrorText
+	EN ErrorText
+}
+
+type ErrorModel struct {
+	ErrorString Errors
+	ErrorCode   ErrorCode
+}
+
+const (
+	InternalServerError = "internal_server_error"
+	UserDoesNotExists   = "user_does_not_exists"
+)
+
+var EngErrors = ErrorText{
+	InternalServerError: "internal server error",
+	UserDoesNotExists:   "user does not exists",
+}
+
+var FaErrors = ErrorText{
+	InternalServerError: "خطای داخلی سرور",
+	UserDoesNotExists:   "کاربر مورد نظر وجود ندارد",
+}
+
+var ErrorCodes = ErrorCode{
+	InternalServerError: http.StatusInternalServerError,
+	UserDoesNotExists:   http.StatusNotFound,
+}
+
+var TheErrors = ErrorModel{
+	ErrorString: Errors{
+		EN: EngErrors,
+		FA: FaErrors,
+	},
+	ErrorCode: ErrorCodes,
+}
